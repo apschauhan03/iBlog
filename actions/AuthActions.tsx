1393d81490739220f32d68be0952cd984f0f0f50
Abhijit Singh <abhijitchau8948@gmail.com>
2024-06-20 19:53:19 +0530

    db connected with prisma

--- actions/AuthActions.tsx
+++ actions/AuthActions.tsx
@@ -1,11 +1,34 @@
 "use server"
-export async function SignUpAction(prevState: any, formData: FormData) {
-    // console.log(formData);
 
+// prisma db
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+// hashing with salting A tier
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+
+interface userType {
+    data: {
+        name: string;
+        email: string;
+        password: string;
+        posts:{}
+    }
+}
+export async function SignUpAction(prevState: any, formData: FormData) {
     const name = formData.get("name")?.toString();
     const email = formData.get("email")?.toString();
     const password = formData.get("password")?.toString();
     const RePassword = formData.get("RePassword")?.toString();
+
+    if (!email || !name || !password) {
+        return {
+            message: "Please provide valid credentials"
+        }
+    }
+
 
     if (name?.length === 0)
         return {
@@ -25,9 +48,27 @@ export async function SignUpAction(prevState: any, formData: FormData) {
         }
 
 
-    console.log('====================================');
-    console.log(name,email,password,RePassword);
-    console.log('====================================');
+    bcrypt.hash(password, saltRounds, async function (err: any, hash: string) {
+        if (err) {
+            console.log("error occured while hashing!", err);
+            return {
+                message: "Password invalid"
+            }
+        }
+
+        // store into our db
+        let newUser: userType = {
+            data: {
+                name: name,
+                email: email,
+                password: hash,
+                posts:{}
+            }
+        }
+        await prisma.iBlogUsers.create(newUser)
+        const allUsers = await prisma.iBlogUsers.findMany();
+        console.log(allUsers);
+    });
     return {
         message: "Account Created Successfully"
     }
